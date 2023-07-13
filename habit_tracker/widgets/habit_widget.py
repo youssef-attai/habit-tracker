9f34c6f40cec133f8d466d924d9bbb058641b6ad
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-07-13 23:08:13 +0300

    add the edit and delete button

--- habit_tracker/widgets/habit_widget.py
+++ habit_tracker/widgets/habit_widget.py
@@ -1,13 +1,30 @@
-from PyQt6.QtWidgets import QLabel
-from core.base_widget import BaseWidget
+from PyQt6.QtWidgets import QLabel, QPushButton
+from core.row_widget import RowWidget
+from state import State
+from navigator import Navigator
+from screens.create_habit_screen import CreateHabitScreen
 
 
-class HabitWidget(BaseWidget):
+class HabitWidget(RowWidget):
     def __init__(self, habit):
         self.habit = habit
         super().__init__()
 
     def build(self):
         super().build()
-        self.label = QLabel(self.habit.name)
+        self.label = QLabel(self.habit.title)
         self.layout().addWidget(self.label)
+
+        self.edit_button = QPushButton("Edit")
+        self.edit_button.clicked.connect(self._edit_button_clicked)
+        self.layout().addWidget(self.edit_button)
+
+        self.delete_button = QPushButton("Delete")
+        self.delete_button.clicked.connect(self._delete_button_clicked)
+        self.layout().addWidget(self.delete_button)
+
+    def _edit_button_clicked(self):
+        Navigator.instance.push(CreateHabitScreen(self.habit))
+
+    def _delete_button_clicked(self):
+        State.instance.habit_controller.delete_habit(self.habit.id)
