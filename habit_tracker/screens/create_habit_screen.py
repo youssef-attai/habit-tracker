74c9ef48c74471ea73e5f79b2ee1c000dc201514
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-07-13 08:30:39 +0300

    add gui elements for rest of habit attributes

--- habit_tracker/screens/create_habit_screen.py
+++ habit_tracker/screens/create_habit_screen.py
@@ -1,6 +1,8 @@
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QLineEdit, QPushButton
+from PyQt6.QtGui import QIntValidator
+from PyQt6.QtWidgets import QComboBox, QLineEdit, QPushButton, QTextEdit
 from core.base_widget import BaseWidget
+from models.habit_model import HabitModel
 from state import State
 from navigator import Navigator
 
@@ -17,6 +19,19 @@ class CreateHabitScreen(BaseWidget):
         self.title.setPlaceholderText("Title")
         self.layout().addWidget(self.title)
 
+        self.description = QTextEdit()
+        self.description.setPlaceholderText("Description")
+        self.layout().addWidget(self.description)
+
+        self.goal = QLineEdit()
+        self.goal.setPlaceholderText("Goal")
+        self.goal.setValidator(QIntValidator())
+        self.layout().addWidget(self.goal)
+
+        self.repeat = QComboBox()
+        self.repeat.addItems(["Daily", "Weekly", "Monthly"])
+        self.layout().addWidget(self.repeat)
+
         self.add_button = QPushButton("Add")
         self.add_button.clicked.connect(self.on_add_button_clicked)
         self.layout().addWidget(self.add_button)
@@ -26,7 +41,14 @@ class CreateHabitScreen(BaseWidget):
         self.layout().addWidget(self.back_button)
 
     def on_add_button_clicked(self):
-        State.instance.habit_controller.create_habit(self.title.text())
+        State.instance.habit_controller.create_habit(
+            HabitModel(
+                self.title.text(),
+                self.description.toPlainText(),
+                int(self.goal.text()),
+                self.repeat.currentText(),
+            )
+        )
         Navigator.instance.pop()
 
     def on_back_button_clicked(self):
