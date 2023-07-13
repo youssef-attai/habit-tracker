bedc1711bb024006e165b23a5250ae18bbb7101a
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-07-13 06:25:15 +0300

    add habit controller and repository instances to global state

--- habit_tracker/state.py
+++ habit_tracker/state.py
@@ -1,5 +1,13 @@
+from controllers.habit_controller import HabitController
+from repositories.habit_repository import DummyHabitRepository
+
+
 class State:
     instance: 'State' = None
+
+    def __init__(self):
+        self.habit_repository = DummyHabitRepository()
+        self.habit_controller = HabitController(self.habit_repository)
 
     @staticmethod
     def init():
