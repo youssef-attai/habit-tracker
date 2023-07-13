8689729d9d25d3a73e501071a94d34418431ad29
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-07-13 23:15:24 +0300

    put habit list in a scroll area

--- habit_tracker/screens/home_screen.py
+++ habit_tracker/screens/home_screen.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QPushButton
+from PyQt6.QtWidgets import QPushButton, QScrollArea
 from PyQt6.QtCore import Qt
 from core.column_widget import ColumnWidget
 from navigator import Navigator
@@ -14,12 +14,17 @@ class HomeScreen(ColumnWidget):
         super().build()
         self.layout().setAlignment(Qt.AlignmentFlag.AlignTop)
 
+        self.scroll_area = QScrollArea()
+        self.scroll_area.setWidgetResizable(True)
+
         self.habit_list_widget = HabitListWidget()
-        self.layout().addWidget(self.habit_list_widget)
+        self.scroll_area.setWidget(self.habit_list_widget)
 
-        self.add_new_habit_button = QPushButton("Add new habit")
-        self.add_new_habit_button.clicked.connect(self.on_add_new_habit_button_clicked)
-        self.layout().addWidget(self.add_new_habit_button)
+        self.layout().addWidget(self.scroll_area)
 
-    def on_add_new_habit_button_clicked(self):
+        self.new_habit_button = QPushButton("New habit")
+        self.new_habit_button.clicked.connect(self._new_habit_button_clicked)
+        self.layout().addWidget(self.new_habit_button)
+
+    def _new_habit_button_clicked(self):
         Navigator.instance.push(CreateHabitScreen())
