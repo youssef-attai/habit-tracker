7561cfa7917c57ae9d2d06156e2a92de15efefec
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-07-13 06:18:01 +0300

    create qt app

--- habit_tracker/main.py
+++ habit_tracker/main.py
@@ -1,4 +1,9 @@
+from PyQt6.QtWidgets import QApplication
+from constants import APP_NAME
 def main():
+    app = QApplication([])
+    app.setApplicationName(APP_NAME)
+    app.exec()
 
 
 if __name__ == "__main__":
